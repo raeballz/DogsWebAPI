c39d3473e512fe3ffb44941e976805ce28801ea6
Andrew Rae <andrew.rae@inps.co.uk>
2019-01-15 23:43:51 +0000

    #6: Added mock method for populating the context from json, however it currently is populating the model with test data manually. Updated the GetAllBreeds method to return an IEnumarable of all dog breeds stored in the context.

--- RestClient/Controllers/DogBreedController.cs
+++ RestClient/Controllers/DogBreedController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DogRestAPI.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace DogRestAPI.Controllers
+﻿namespace DogRestAPI.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DogRestAPI.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
     /// <summary>
     /// Main logic for REST API functions.
     /// </summary>
@@ -23,8 +23,34 @@ namespace DogRestAPI.Controllers
 
             if (this.controllerContext.DogBreedItemList.Count() == 0)
             {
-                ///TODO: If not populated, read in Json Payload
+                PopulateContextFromJson();
             }
+        }
+
+        private void PopulateContextFromJson()
+        {
+            ///TODO: If not populated, read in Json Payload
+            ///But just create these objects for now.
+            var hound0 = controllerContext.DogBreedItemList.Add( new DogBreedItem { Id = 1, BreedName = "Hound0", SubBreed = new List<DogSubBreed>()});
+            var hound1 = controllerContext.DogBreedItemList.Add( new DogBreedItem { Id = 2, BreedName = "Hound1", SubBreed = new List<DogSubBreed>()});
+
+            ///Push changes up so entity db can add a sub-breed to each DogBreed
+            controllerContext.SaveChanges();
+
+            controllerContext.DogBreedItemList.First(x => x.Id == 1).SubBreed.Add (new DogSubBreed { SubBreedName = "SubBreed0"});
+            controllerContext.DogBreedItemList.First(x => x.Id == 1).SubBreed.Add (new DogSubBreed { SubBreedName = "SubBreed1" });
+
+            controllerContext.DogBreedItemList.First(x => x.Id == 2).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed2" });
+            controllerContext.DogBreedItemList.First(x => x.Id == 2).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed3" });
+
+            controllerContext.SaveChanges();
+        }
+
+        public IEnumerable<DogBreedItem> GetAllBreeds()
+        {
+            ///still stubbed
+            throw new NotImplementedException();
+            return controllerContext.DogBreedItemList.ToList();
         }
 
         private void DeleteBreedByName()
@@ -53,11 +79,6 @@ namespace DogRestAPI.Controllers
         }
 
         private void GetBreedByID()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void GetAllBreeds()
         {
             throw new NotImplementedException();
         }
