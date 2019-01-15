33d6c047d165f7e45ff079288cce310162f1f27c
Andrew Rae <andrew.rae@inps.co.uk>
2019-01-15 21:21:55 +0000

    #2: Add a basic outline for parsing a dog item, include a list of subbreeds. Not too sure if we're needing the ID, or if the breed name will be enough; I woudn't have done the payload this way to be honest, dog breed would be an object, with the name of each dog being the primary key within that object. The payload would then return the list of dogs meaning you could parse them out into quite  a nice representation easily.

---
+++ RestClient/Models/DogBreedItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogRestAPI.Models
+{
+    /// <summary>
+    /// First sweep at a possible model for JSON deserialisation
+    /// </summary>
+    public class DogBreedItem
+    {
+        //Index of dog in list? Might not be needed, unique ID is probably breedName
+        public long Id { get; set; }
+
+        //Name in json property
+        public string BreedName { get; set; }
+
+        //List of sub-breeds in dog breed
+        public List<string> SubBreed{ get; set; }
+    }
+}
