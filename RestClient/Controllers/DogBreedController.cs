0c373c53a9c2759e23ecb09f9bf5cee1985d650a
Andrew Rae <andrew.rae@inps.co.uk>
2019-01-15 21:35:13 +0000

    #4: Created dog breed controller, added a bunch of stubbed methods for functionality. Created a local context DB for us to store our data in whilst the application is live.

---
+++ RestClient/Controllers/DogBreedController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogRestAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogRestAPI.Controllers
+{
+    /// <summary>
+    /// Main logic for REST API functions.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogBreedController : ControllerBase
+    {
+        private readonly DogBreedContext controllerContext;
+
+        public DogBreedController(DogBreedContext context)
+        {
+            this.controllerContext = context;
+
+            if (this.controllerContext.DogBreedItemList.Count() == 0)
+            {
+                ///TODO: If not populated, read in Json Payload
+            }
+        }
+
+        private void DeleteBreedByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteBreedByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PutBreed()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PostBreed()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetBreedByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetBreedByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetAllBreeds()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestClient/Models/DogBreedContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogRestAPI.Models
+{
+    /// <summary>
+    /// Controls the database context via Entity Framework
+    /// </summary>
+    public class DogBreedContext : DbContext
+    {
+        public DogBreedContext(DbContextOptions<DogBreedContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DogBreedItem> DogBreedItemList { get; set; }
+    }
+}
