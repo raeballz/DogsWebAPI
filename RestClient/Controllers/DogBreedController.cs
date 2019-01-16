9aa92a15803a1f1b55c120251d233f45ca7fcdfa
Andrew Rae <andrew.rae@inps.co.uk>
2019-01-16 20:42:07 +0000

    #12 Pare down routes at the moment so we're only using the unique identity ID rather than by identy AND by name

--- RestClient/Controllers/DogBreedController.cs
+++ RestClient/Controllers/DogBreedController.cs
@@ -98,31 +98,24 @@
                 return NotFound();
             }
         }
+
+        //[HttpGet("{breedname}")]
+        //public async Task<ActionResult<DogBreedItem>> GetBreedByName(string breedName)
+        //{
+        //    DogBreedItem requestedBreed = await controllerContext.DogBreedItemList.FirstAsync(x => x.BreedName == breedName);
+
+        //    if (requestedBreed == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        return Ok(requestedBreed);
+        //    }
+        //}
         #endregion
 
         #region HTTPDelete
-        /// <summary>
-        /// Method searches for breed by name, then removes it
-        /// </summary>
-        /// <param name="breedName">Name of the breed we wish to delete</param>
-        /// <returns>204 If Successful, 404 Not found If Unsuccesful</returns>
-        [HttpDelete("{breedname}")]
-        public async Task<ActionResult<DogBreedItem>> DeleteBreedByName(string breedName)
-        {
-            DogBreedItem breedToDelete;
-            try
-            {
-                breedToDelete = await controllerContext.DogBreedItemList.FirstAsync(x => x.BreedName == breedName);
-                controllerContext.DogBreedItemList.Remove(breedToDelete);
-                await controllerContext.SaveChangesAsync();
-                return Ok(breedToDelete);
-            }
-            catch
-            {
-                return NotFound();
-            }
-        }
-
         /// <summary>
         /// HTTP Method to delete breed via ID
         /// </summary>
@@ -144,6 +137,28 @@
                 return NotFound();
             }
         }
+
+        ///// <summary>
+        ///// Method searches for breed by name, then removes it
+        ///// </summary>
+        ///// <param name="breedName">Name of the breed we wish to delete</param>
+        ///// <returns>204 If Successful, 404 Not found If Unsuccesful</returns>
+        //[HttpDelete("{breedname}")]
+        //public async Task<ActionResult<DogBreedItem>> DeleteBreedByName(string breedName)
+        //{
+        //    DogBreedItem breedToDelete;
+        //    try
+        //    {
+        //        breedToDelete = await controllerContext.DogBreedItemList.FirstAsync(x => x.BreedName == breedName);
+        //        controllerContext.DogBreedItemList.Remove(breedToDelete);
+        //        await controllerContext.SaveChangesAsync();
+        //        return Ok(breedToDelete);
+        //    }
+        //    catch
+        //    {
+        //        return NotFound();
+        //    }
+        //}
         #endregion
 
         private void PutBreed()
@@ -155,20 +170,5 @@
         {
             throw new NotImplementedException();
         }
-
-        //[HttpGet("{breedname}")]
-        //public async Task<ActionResult<DogBreedItem>> GetBreedByName(string breedName)
-        //{
-        //    DogBreedItem requestedBreed = await controllerContext.DogBreedItemList.FirstAsync(x => x.BreedName == breedName);
-
-        //    if (requestedBreed == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    else
-        //    {
-        //        return Ok(requestedBreed);
-        //    }
-        //}
     }
 }
