453e9feffff76c69620abfc204dc0fe3562d88be
Andrew Rae <andrew.rae@inps.co.uk>
2019-01-16 20:43:25 +0000

    #12 Small tweak to make the indexing more sensible on the test data.

--- RestClient/Controllers/DogBreedController.cs
+++ RestClient/Controllers/DogBreedController.cs
@@ -43,18 +43,18 @@
         {
             ///TODO: If not populated, read in Json Payload
             ///But just create these objects for now.
-            controllerContext.DogBreedItemList.Add(new DogBreedItem { Id = 1, BreedName = "Hound0", SubBreed = new List<DogSubBreed>() });
-            controllerContext.DogBreedItemList.Add(new DogBreedItem { Id = 2, BreedName = "Hound1", SubBreed = new List<DogSubBreed>() });
+            controllerContext.DogBreedItemList.Add(new DogBreedItem { Id = 1, BreedName = "Hound1", SubBreed = new List<DogSubBreed>() });
+            controllerContext.DogBreedItemList.Add(new DogBreedItem { Id = 2, BreedName = "Hound2", SubBreed = new List<DogSubBreed>() });
 
             ///Push changes up so entity db can add a sub-breed to each DogBreed
             controllerContext.SaveChangesAsync();
 
             ///Populate sub-breeds in current breed dataset.
-            controllerContext.DogBreedItemList.First(x => x.Id == 1).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed0" });
-            controllerContext.DogBreedItemList.First(x => x.Id == 1).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed1" });
+            controllerContext.DogBreedItemList.First(x => x.Id == 1).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed1.1" });
+            controllerContext.DogBreedItemList.First(x => x.Id == 1).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed1.2" });
 
-            controllerContext.DogBreedItemList.First(x => x.Id == 2).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed2" });
-            controllerContext.DogBreedItemList.First(x => x.Id == 2).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed3" });
+            controllerContext.DogBreedItemList.First(x => x.Id == 2).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed2.1" });
+            controllerContext.DogBreedItemList.First(x => x.Id == 2).SubBreed.Add(new DogSubBreed { SubBreedName = "SubBreed2.2" });
 
             controllerContext.SaveChangesAsync();
         }
