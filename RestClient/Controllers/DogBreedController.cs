29b9169a81f51614a0a65ab4091cb04c19517a5e
Andrew Rae <andrew.rae@inps.co.uk>
2019-01-18 19:45:20 +0000

    #20: Dog breed controller will now populate from json file that the client gave us. The data structure provided by the client is good for human readability, but needed converted to something more robust for a relational database. This large migration is only ran once when the app is started for the first time. The rest of the calls are done on the database structure. JSON file included in sln.

--- RestClient/Controllers/DogBreedController.cs
+++ RestClient/Controllers/DogBreedController.cs
@@ -2,11 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using DogRestAPI.Models;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     /// <summary>
     /// Main logic for REST API functions.
@@ -39,35 +42,80 @@
         /// Called on inital run of application. 
         /// Populates the application context via JSON file.
         /// </summary>
-        private void PopulateContextFromJson()
+        private async void PopulateContextFromJson()
         {
-            ///TODO: If not populated, read in Json Payload
-            ///But just create these objects for now.
-            List<DogBreedItem> dogBreeds = new List<DogBreedItem>
+            using (StreamReader r = new StreamReader("dogs.json"))
             {
-                new DogBreedItem { DogBreedItemId = 1, BreedName = "Hound1" },
-                new DogBreedItem { DogBreedItemId = 2, BreedName = "Hound2" }
-            };
+                string json = r.ReadToEnd();
+                dynamic jsonObject = JsonConvert.DeserializeObject(json);
+                Dictionary<string, string[]> jsonDictionary = new Dictionary<string, string[]>();
 
-            dogBreeds.ForEach(breed => dataContext.DogBreedItemList.Add(breed));
-            dataContext.SaveChanges();
+                foreach (var dogBreedProperty in jsonObject)
+                {
+                    if(dogBreedProperty.Value.Count != 0)
+                    {
+                        string[] subBreedArray = new string[dogBreedProperty.Value.Count];
+                        int counter = 0;
 
-            List<DogSubBreed> subBreeds = new List<DogSubBreed>
+                        foreach(string x in dogBreedProperty.Value)
+                        {
+                            subBreedArray[counter] = x;
+                            counter++;
+                        }          
+
+                        jsonDictionary.TryAdd(dogBreedProperty.Name, subBreedArray); 
+                    }
+                    else
+                    {
+                        jsonDictionary.TryAdd(dogBreedProperty.Name, new string[0]);
+                    }
+                }
+
+                //Dictionary<string, string[]> jsonDictionary = new Dictionary<string, string[]>() {  };
+
+                foreach (string breedKey in jsonDictionary.Keys)
+                {
+                    await PopulateNewDogBreed(breedKey);
+                }
+
+                int breedId = 1;
+                foreach (string[] subBreeds in jsonDictionary.Values)
+                {
+                    await PopulateNewDogBreedSubbreeds(subBreeds, breedId);
+                    breedId++;
+                }
+            }            
+        }
+
+        private async Task PopulateNewDogBreedSubbreeds(string[] dogSubbreedArray, long parentBreedId)
+        {
+            DogBreedItem parentBreed = dataContext.DogBreedItemList.Find(parentBreedId);
+            
+            //Add each of the sub-breeds to the sub breed DBSet
+            foreach (string subBreedName in dogSubbreedArray)
             {
-                new DogSubBreed {  ParentBreedId = dataContext.DogBreedItemList.Find((long)1).DogBreedItemId, SubBreedName = "Hound1.1"},
-                new DogSubBreed { ParentBreedId = dataContext.DogBreedItemList.Find((long)1).DogBreedItemId, SubBreedName = "Hound1.2" },
-                new DogSubBreed { ParentBreedId = dataContext.DogBreedItemList.Find((long)2).DogBreedItemId, SubBreedName = "Hound2.1" },
-                new DogSubBreed { ParentBreedId = dataContext.DogBreedItemList.Find((long)2).DogBreedItemId, SubBreedName = "Hound2.2" }
-            };
+                DogSubBreed subBreed = new DogSubBreed() { ParentBreedId = parentBreedId, SubBreedName = subBreedName };
+                dataContext.DogSubBreedItemList.Add(subBreed);
+            }
 
-            subBreeds.ForEach(subBreed => dataContext.DogSubBreedItemList.Add(subBreed));
-            dataContext.SaveChanges();
+            //Now save DB context so that we can retreive them with a generated ID.
+            await dataContext.SaveChangesAsync();
 
-            dataContext.DogBreedItemList.First(breed => breed.DogBreedItemId == 1).SubBreeds = new List<DogSubBreed>() { dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 1), dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 2)};
-            dataContext.DogBreedItemList.First(breed => breed.DogBreedItemId == 2).SubBreeds = new List<DogSubBreed>() { dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 3), dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 4)};
+            //Build a list of all sub breeds where the sub-subbreed's parent ID is the parent Id we're looking for.
+            List<DogSubBreed> subBreeds = dataContext.DogSubBreedItemList.Where(x => x.ParentBreedId == parentBreedId).ToList();
 
-            ///Push changes up so entity db can add a sub-breed to each DogBreed
-            dataContext.SaveChanges();
+            //set it in the context using our reference
+            dataContext.DogBreedItemList.Find(parentBreedId).SubBreeds = subBreeds;
+            await dataContext.SaveChangesAsync();
+        }
+
+        private async Task PopulateNewDogBreed(string breedName)
+        {
+            DogBreedItem dogBreed = new DogBreedItem();
+            dogBreed.BreedName = breedName;
+
+            dataContext.DogBreedItemList.Add(dogBreed);
+            await dataContext.SaveChangesAsync();
         }
 
         #region HTTPGet
@@ -179,5 +227,36 @@
             return Ok();
         }
         #endregion
+
+        private void PopulateWithTestData()
+        {
+            ///TODO: If not populated, read in Json Payload
+            ///But just create these objects for now.
+            List<DogBreedItem> dogBreeds = new List<DogBreedItem>
+            {
+                new DogBreedItem { DogBreedItemId = 1, BreedName = "Hound1" },
+                new DogBreedItem { DogBreedItemId = 2, BreedName = "Hound2" }
+            };
+
+            dogBreeds.ForEach(breed => dataContext.DogBreedItemList.Add(breed));
+            dataContext.SaveChanges();
+
+            List<DogSubBreed> subBreeds = new List<DogSubBreed>
+            {
+                new DogSubBreed {  ParentBreedId = dataContext.DogBreedItemList.Find((long)1).DogBreedItemId, SubBreedName = "Hound1.1"},
+                new DogSubBreed { ParentBreedId = dataContext.DogBreedItemList.Find((long)1).DogBreedItemId, SubBreedName = "Hound1.2" },
+                new DogSubBreed { ParentBreedId = dataContext.DogBreedItemList.Find((long)2).DogBreedItemId, SubBreedName = "Hound2.1" },
+                new DogSubBreed { ParentBreedId = dataContext.DogBreedItemList.Find((long)2).DogBreedItemId, SubBreedName = "Hound2.2" }
+            };
+
+            subBreeds.ForEach(subBreed => dataContext.DogSubBreedItemList.Add(subBreed));
+            dataContext.SaveChanges();
+
+            dataContext.DogBreedItemList.First(breed => breed.DogBreedItemId == 1).SubBreeds = new List<DogSubBreed>() { dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 1), dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 2) };
+            dataContext.DogBreedItemList.First(breed => breed.DogBreedItemId == 2).SubBreeds = new List<DogSubBreed>() { dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 3), dataContext.DogSubBreedItemList.First(breed => breed.DogSubBreedId == 4) };
+
+            ///Push changes up so entity db can add a sub-breed to each DogBreed
+            dataContext.SaveChanges();
+        }
     }
 }
